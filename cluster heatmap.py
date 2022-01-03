0c83434d62ed4281f9ef375e4f79ebcb52859ea5
wun-yu-lin <t928394558@gmail.com>
2022-01-03 22:05:57 +0800

    2022_01_03

--- cluster heatmap.py
+++ cluster heatmap.py
@@ -11,21 +11,21 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 excel_file_path = input("請給檔案位置:")
-heatmap_data=pd.read_excel(excel_file_path, index_col="Row.names")
+heatmap_data=pd.read_excel(excel_file_path, index_col="Feature ID")
 
 species = heatmap_data.iloc[0,0:]
-dicts = dict(zip(species.unique(), "rb"))
+dicts = dict(zip(species.unique(), "bg"))
 col_colors = species.map(dicts)
-heatmap_data = heatmap_data.drop(index="species")
+heatmap_data = heatmap_data.drop(index="Class")
 heatmap_data = heatmap_data.astype(int)
 sns.clustermap(heatmap_data,
                 row_cluster=True,
                 col_cluster=True,
                 cmap="coolwarm",
                 z_score=0,
-                figsize=(30,34),
+                figsize=(15,34),
                 col_colors=col_colors
                  )
-plt.savefig("heatmap_30_34.png",dpi= 1000)
+plt.savefig("heatmap_15_34_negative.png",dpi= 500)
 
 
